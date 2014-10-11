dc038fb10058fc0ecdbc7c34001456a35297eb8d
Adrian Quek <adrianqsk@gmail.com>
2014-10-11 17:57:05 +0800

    Added tests for css, js, img, posts subdirs in 'public'

--- test/common/utils_test.clj
+++ test/common/utils_test.clj
@@ -10,4 +10,8 @@
       (create tmp)
       (is (fs/directory? tmp))
       (is (fs/directory? (str tmp "/public")))
+      (is (fs/directory? (str tmp "/public/css")))
+      (is (fs/directory? (str tmp "/public/img")))
+      (is (fs/directory? (str tmp "/public/js")))
+      (is (fs/directory? (str tmp "/public/posts")))
       (fs/delete-dir tmp))))
