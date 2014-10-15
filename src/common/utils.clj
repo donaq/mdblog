e68e7a71729567fb0ed56c1fe122ac5c0e0b422c
Adrian Quek <adrianqsk@gmail.com>
2014-10-15 20:18:12 +0800

    Renamed copy-public to copy-public-file. Wrote copy-public-jar so that site creation can be done using uberjar.

--- src/common/utils.clj
+++ src/common/utils.clj
@@ -2,10 +2,12 @@
   (:gen-class)
   (:require [clojure.data.json :as json]
             [clojure.java.io :as io]
-            [me.raynes.fs :as fs]))
+            [me.raynes.fs :as fs])
+  (:import (java.util.zip ZipFile ZipEntry)))
 
-(defn copy-public
-  "Recursively copies public resources to target dir"
+; so this function is probably totally fucking useless now
+(defn copy-public-file
+  "Recursively copies public resources to target dir. This is only useful when running from lein."
   [dir resourcename]
   (let [resource (-> resourcename io/resource io/file)]
     ; directory recursive call
@@ -15,15 +17,40 @@
         (fs/mkdir subdir)
         ; for each file in directory, do copy public
         (doseq [fname files]
-          (copy-public subdir (str resourcename "/" fname))))
+          (copy-public-file subdir (str resourcename "/" fname))))
       ; copy
       (io/copy resource (-> (str dir "/" (.getName resource)) io/file)))))
+
+(defn copy-public-jar
+  "Copies public resources from jar to target dir. Because java is fucking retarded."
+  [dir resourcename]
+  (let [jarname (-> (java.lang.ClassLoader/getSystemClassLoader) .getURLs first .getPath)
+        ^ZipFile zf (ZipFile. jarname)
+        ; get entries in public/
+        entries (vec (for [^ZipEntry entry (enumeration-seq (.entries zf))
+                  :let [ename (.getName entry)]
+                  :when (.startsWith ename "public")]
+                  ename))]
+    (.close zf)
+    (doseq [entry entries]
+      ; ending with / means directory in zipfile format
+      (if (.endsWith entry "/")
+        (fs/mkdirs (str dir "/" entry))
+        (io/copy (-> entry io/resource .getContent) (io/file (str dir "/" entry)))))))
+
+(defn copy-public
+  "Determine if protocol is jar"
+  [dir resourcename]
+  ; file
+  (if (not (= "jar" (-> resourcename io/resource .getProtocol)))
+    (copy-public-file dir resourcename)
+    (copy-public-jar dir resourcename)))
 
 (defn create
   "Does the actual creation. Takes a directory name and creates a site directory, copying all the static files into it if it does not exist."
   [dir]
   (if (fs/directory? dir)
-    (print "Directory " dir " already exists")
+    (println (str "Directory " dir " already exists"))
     (do
       (fs/mkdir dir)
       (copy-public dir "public"))))
