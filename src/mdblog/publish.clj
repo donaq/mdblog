aa9e7c42680576b04fbb450806ce0f6fd32de4dc
Adrian Quek <adrianqsk@gmail.com>
2014-10-18 20:03:44 +0800

    Added publish-to-non-existent test

--- src/mdblog/publish.clj
+++ src/mdblog/publish.clj
@@ -2,10 +2,17 @@
 (ns mdblog.publish
   (:gen-class)
   (:require [common.utils :as utils]
+            [clojure.data.json :as json]
             [me.raynes.fs :as fs]))
 
 (defn publish
   [& args]
   (if (< (count args) 3)
     (println "Usage: publish <site directory> <filename> <Title>")
-    (println "publish")))
+    (let [site (nth args 0)
+          fname (nth args 1)
+          title (nth args 2)
+          dat (-> (str site "/public/posts/index.json") slurp json/read-str)
+          created (System/currentTimeMillis)]
+      (println dat)
+      )))
--- test/mdblog/publish_test.clj
+++ test/mdblog/publish_test.clj
@@ -1,9 +1,28 @@
 (ns mdblog.publish-test
   (:require [clojure.test :refer :all]
             [mdblog.publish :refer :all]
+            [common.utils :refer :all]
             [clojure.java.io :as io]
             [me.raynes.fs :as fs]))
 
+(def tmp "tmp")
+
+(defn create-tmp
+  "fixture to create site in tmp"
+  [f]
+  (create tmp)
+  (f)
+  (fs/delete-dir tmp))
+
 (deftest publish-test
   (testing "file written and public/posts/index.json updated"
-    (apply publish ["tmp" "file.md" "some title"])))
+    (publish tmp "file.md" "some title")))
+
+(deftest publish-to-non-existent
+  (testing "trying to publish to non-existent site should create that site"
+    (fs/delete-dir tmp)
+    (publish tmp "file.md" "some title")
+    (is (fs/directory? tmp))))
+    
+
+(use-fixtures :each create-tmp)
